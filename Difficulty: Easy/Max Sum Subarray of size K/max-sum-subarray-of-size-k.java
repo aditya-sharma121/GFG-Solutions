69cb672aa5877183f04d1a305588c681cd7b361a
Aditya sharma <jaimaachaumukha@gmail.com>
2026-09-11 13:35:00 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Max Sum Subarray of size K/max-sum-subarray-of-size-k.java
+++ Difficulty: Easy/Max Sum Subarray of size K/max-sum-subarray-of-size-k.java
@@ -1,16 +1,16 @@
 class Solution {
-    public int maxSubarraySum(int[] arr, int k) {
+    public int maxSubarraySum(int[] nums, int k) {
         // Code here
-        int n = arr.length;
-        int max = Integer.MIN_VALUE;
+         int n = nums.length;
+         int max = Integer.MIN_VALUE;
          int sum=0;
          for(int i=0;i<n;i++){
-             sum += arr[i];
-             if(i<k-1) continue;
+             sum += nums[i];
              
+             if(i<k-1) continue;
              max = Math.max(max,sum);
              int sin = i-k+1;
-             sum -= arr[sin];
+             sum -= nums[sin];
          }
          return max;
     }
